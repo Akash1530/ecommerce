e45d4e57f873ccf94d6abd7a8c8d21eac06f1c2f
akash <akashkumarrana1530@gmail.com>
2022-07-21 17:04:16 +0530

    admin

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCB-h_T2ZL-a7xt5zMvZmSh8GBXtawzL0c",
-  authDomain: "shop-889c6.firebaseapp.com",
-  projectId: "shop-889c6",
-  storageBucket: "shop-889c6.appspot.com",
-  messagingSenderId: "105110075028",
-  appId: "1:105110075028:web:7cee5adf1ed39f597e6681",
+  apiKey: 'AIzaSyCygx4KaQNKCIpRTej17r2Gr_8BkkJF18I',
+  authDomain: 'admin-c3265.firebaseapp.com',
+  projectId: 'admin-c3265',
+  storageBucket: 'admin-c3265.appspot.com',
+  messagingSenderId: '883538701450',
+  appId: '1:883538701450:web:8a9d2aa3fe19b2b2119173',
 };
 
 // Initialize Firebase
